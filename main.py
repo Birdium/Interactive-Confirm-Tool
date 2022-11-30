8c8e2cdaf3fa19a876b35657c9db0b4a63a5abf8
Birdium <bibishu2001@163.com>
2022-12-01 07:45:18 +0800

    runnable

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import os.path
 import sys
 from input import Input
 from output import Output
-# from PyQt5.QtCore import *
-from PyQt5 import QtCore
+from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 from program import Progpair
 from equality import Equality
-from queue import Queue
+from collections import deque
 import ui
 
 os.putenv("QTWEBENGINE_CHROMIUM_FLAGS", "--no-sandbox")
+
 
 class ConfirmTool(QMainWindow, ui.Ui_MainWindow):
 
@@ -21,10 +21,11 @@ class ConfirmTool(QMainWindow, ui.Ui_MainWindow):
         self.setupUi(self)
         self.eq_list = []
         self.neq_list = []
+        self.worklist: deque = None
         self.eq_pairs = []
-        self.worklist: Queue = Queue()
-        self.human_verified = []
-        self.doubt = []
+        self.neq_pairs = []
+        self.human_verified_pairs = []
+        self.doubt_pairs = []
         self.current_pair: Progpair = None
         self.load_path = os.path.join(os.getcwd())
         self.export_path = os.path.join(os.getcwd())
@@ -33,35 +34,96 @@ class ConfirmTool(QMainWindow, ui.Ui_MainWindow):
         self.notsure_button.clicked.connect(lambda: self.press(Equality.DOUBT))
         self.actionImport_from.triggered.connect(lambda: self.load(self.load_path))
         self.actionExport_to.triggered.connect(lambda: self.export(self.export_path))
+        self.waiting_listview.itemClicked.connect(self.item_click_judged)
+        self.nequal_listview.itemClicked.connect(self.item_click_neq)
+        self.equal_listview.itemClicked.connect(self.item_click_verified)
+        self.nsure_listview.itemClicked.connect(self.item_click_doubt)
+        self.item_map = {}
+
+    def item_click_judged(self):
+        item = self.waiting_listview.selectedItems()[0]
+        clicked_pair = self.item_map[item.text()]
+        if self.current_pair is not None and self.current_pair is not clicked_pair:
+            self.worklist.appendleft(self.current_pair)
+            self.current_pair = clicked_pair
+        self.display()
+
+    def item_click_neq(self):
+        item = self.nequal_listview.selectedItems()[0]
+        clicked_pair = self.item_map[item.text()]
+        if self.current_pair is not None and self.current_pair is not clicked_pair:
+            self.worklist.appendleft(self.current_pair)
+        self.current_pair = clicked_pair
+        self.display()
+
+    def item_click_verified(self):
+        item = self.equal_listview.selectedItems()[0]
+        clicked_pair = self.item_map[item.text()]
+        if self.current_pair is not None and self.current_pair is not clicked_pair:
+            self.worklist.appendleft(self.current_pair)
+            self.current_pair = clicked_pair
+        self.display()
+
+    def item_click_doubt(self):
+        item = self.nsure_listview.selectedItems()[0]
+        clicked_pair = self.item_map[item.text()]
+        if self.current_pair is not None and self.current_pair is not clicked_pair:
+            self.worklist.appendleft(self.current_pair)
+            self.current_pair = clicked_pair
+        self.display()
 
     def get_next(self):
-        if not self.worklist.empty():
-            self.current_pair = self.worklist.get()
-            self.show_diff(self.current_pair)
+        if len(self.worklist) > 0:
+            self.current_pair = self.worklist.popleft()
+            self.display()
+        widget2list = [
+            [self.equal_listview, self.human_verified_pairs],
+            [self.nequal_listview, self.neq_pairs],
+            [self.waiting_listview, self.eq_pairs],
+            [self.nsure_listview, self.doubt_pairs]
+        ]
+        self.item_map = {}
+        for widget, pair_list in widget2list:
+            widget.clear()
+            for pair in pair_list:
+                item_str = pair.prog1 + "," + pair.prog2
+                item = QListWidgetItem(pair.prog1 + "," + pair.prog2)
+                self.item_map[item_str] = pair
+                widget.addItem(item)
 
     def press(self, equality: Equality):
-        if not self.current_pair is None:
+        if self.current_pair is not None:
             self.current_pair.eq = equality
+            for pairlist in [self.eq_pairs, self.neq_pairs, self.human_verified_pairs, self.doubt_pairs]:
+                if self.current_pair in pairlist:
+                    pairlist.remove(self.current_pair)
+
             if equality == Equality.HUMAN_VERIFIED:
-                self.human_verified.append(self.current_pair)
+                self.human_verified_pairs.append(self.current_pair)
             elif equality == Equality.NOT_EQUAL:
-                self.neq_list.append(self.current_pair)
+                self.neq_pairs.append(self.current_pair)
             elif equality == Equality.DOUBT:
-                self.doubt.append(self.current_pair)
+                self.doubt_pairs.append(self.current_pair)
             self.get_next()
 
-    def show_diff(self, progpair):
-        diff = progpair.diff()
-        self.diff_render.setHtml(diff)
+    def display(self):
+        if self.current_pair is not None:
+            diff = self.current_pair.diff()
+            self.prog1_text.setText(self.current_pair.prog1)
+            self.prog2_text.setText(self.current_pair.prog2)
+            self.diff_render.setHtml(diff)
 
     def load(self, fname):
         eq_name = os.path.join(fname, 'input/equal.csv')
         neq_name = os.path.join(fname, 'input/inequal.csv')
         self.eq_list = Input.read(eq_name)
         self.neq_list = Input.read(neq_name)
+        self.item_map = {}
         self.eq_pairs = [Progpair(pair[0], pair[1], Equality.EQUAL_M) for pair in self.eq_list]
-        for pair in self.eq_pairs:
-            self.worklist.put(pair)
+        self.neq_pairs = [Progpair(pair[0], pair[1], Equality.NOT_EQUAL) for pair in self.neq_list]
+        self.human_verified_pairs = []
+        self.doubt_pairs = []
+        self.worklist = deque(self.eq_pairs)
         self.get_next()
 
     def export(self, fname):
